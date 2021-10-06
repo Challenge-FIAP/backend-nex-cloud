5413055b8dc86a73ed082289d9fb12c1b4615fdd
Matheus <matheuscx@outlook.com>
2021-10-06 15:44:40 -0300

    Setando rotas para verbos http

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,22 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::get('user/', [
+    UserController::class, 'index'
+]);
+
+Route::get('user/{id}', [
+    UserController::class, 'show'
+]);
+
+Route::post('user/', [
+    UserController::class, 'store'
+]);
+
+Route::put('user/{id}', [
+    UserController::class, 'update'
+]);
+
+Route::delete('user/{id}', [
+    UserController::class, 'destroy'
+]);
