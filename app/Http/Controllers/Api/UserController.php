4cca07f478e9bdc36bcca437034480e862fa65dc
Matheus <matheuscx@outlook.com>
2021-10-06 19:49:55 -0300

    Recurso e rota que exibe todos os usuários

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -3,18 +3,17 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\UserResource;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 
 class UserController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
+    public function index(): AnonymousResourceCollection
     {
-        
+        $user = User::paginate(15);
+        return UserResource::collection($user);
     }
 
     /**
