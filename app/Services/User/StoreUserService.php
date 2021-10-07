f763241475f63d46120d129f89ea9b634afc73ba
Matheus <matheuscx@outlook.com>
2021-10-06 21:31:46 -0300

    Serviço que cadastra um novo usuário

---
+++ app/Services/User/StoreUserService.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Services\User;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class StoreUserService
+{
+    private Request $request;
+
+    private bool $status;
+
+    private User $user;
+    private string $date;
+    private string $uID;
+
+    public function __construct()
+    {
+        $this->status = true;
+    }
+
+    public function handle(Request $request): void
+    {
+        $this->request = $request;
+
+        $this
+            ->proceed();
+    }
+
+    private function proceed(): void
+    {
+        $this
+            ->setDate();
+
+        // Early return
+        if (!$this->status) {
+            return;
+        }
+
+        $this
+            ->makeUid()
+            ->persistUser();
+    }
+
+    private function setDate(): void
+    {
+        $apiDate = $this->request->date;
+
+        if (!$apiDate) {
+            $this->status = false;
+            return;
+        }
+
+        $this->date = $apiDate;
+    }
+
+    private function makeUid(): StoreUserService
+    {
+        $uID = substr(md5(rand(1, 100)), 0, 10);
+        $uID .= '-';
+        $uID .= substr(md5(rand(1, 100)), 0, 10);
+        $uID .= '-';
+        $uID .= substr(md5(rand(1, 100)), 0, 10);
+
+        $this->uID = $uID;
+
+        return $this;
+    }
+
+    private function persistUser(): StoreUserService
+    {
+        $this->user = User::create([
+            'uid'                   => $this->uID,
+            'date_accept_terms'     => $this->date,
+        ]);
+        return $this;
+    }
+
+    public function getUser(): User
+    {
+        return $this->user;
+    }
+
+    public function status(): bool
+    {
+        return $this->status;
+    }
+}
