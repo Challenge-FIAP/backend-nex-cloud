c5932ccce358e31be3b74207c4554d2aee6c1e0f
Matheus <matheuscx@outlook.com>
2021-10-06 22:43:57 -0300

    Adicionando coluna auto incrementada

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,6 +14,7 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
+            $table->id();
             $table->string('uid')->unique();
             $table->string('date_accept_terms')->nullable();
             $table->string('social_name')->nullable();
