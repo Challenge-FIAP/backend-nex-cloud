f0913a472862ee3fbe600fae6d82b56b21dc5782
Matheus <matheuscx@outlook.com>
2021-10-06 21:30:52 -0300

    Criando migration de usuário

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,11 +14,20 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('uid')->unique();
+            $table->string('date_accept_terms')->nullable();
+            $table->string('social_name')->nullable();
+            $table->string('name')->nullable();
+            $table->string('email')->nullable();
+            $table->string('birth_date')->nullable();
+            $table->string('mother_name')->nullable();
+            $table->string('marital_status')->nullable();
+            $table->string('education_level')->nullable();
+            $table->integer('phone')->nullable();
+            $table->integer('document')->nullable();
+            $table->integer('address')->nullable();
+            $table->integer('score')->nullable();
+            $table->string('password')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
