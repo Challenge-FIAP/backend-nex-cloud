5aa1f24d8e42db4b2b8dc1f776379610fea35929
Matheus <matheuscx@outlook.com>
2021-10-08 16:06:19 -0300

    Criando migration de validação de e-mail

---
+++ database/migrations/2021_10_08_185906_create_email_confirmations_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEmailConfirmationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('email_confirmations', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->unique();
+            $table->string('code')->unique();
+            $table->boolean('valid')->default(false);
+            $table->timestamps();
+
+            $table
+                ->foreign('user_id')
+                ->references('id')
+                ->on('users')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('email_confirmations');
+    }
+}
