56470df5e645523a0798a5c1b0aeeb8472656932
Matheus <matheuscx@outlook.com>
2021-10-08 16:06:50 -0300

    Criando modelo de validação de e-mail e de relacionando com usuário

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -86,6 +87,15 @@ class User extends Authenticatable
                 Phone::class,
                 'id',
                 'phone_id'
+            );
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this
+            ->belongsTo(
+                EmailConfirmation::class,
+                'id'
             );
     }
 }
